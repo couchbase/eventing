8b913b94b8bf021bbddfa9edba3b85a8cfd5048e
Abhishek Singh <singhabhishek.bit@gmail.com>
2017-09-04 09:14:25 +0530

    Handle errors from metakv streaming endpoint
    Observe API exposed by metakv is basically listening on streaming HTTP
    endpoint exposed by ns_server. Connection could get dropped for
    different reasons and if not handled properly, it would affect rebalance
    flow severely.
    
    Change-Id: I559063da451fe4d3ca0aa79198db439196f1965a

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"os"
+	"time"
 
 	"github.com/couchbase/cbauth/metakv"
 	"github.com/couchbase/eventing/supervisor"
+	"github.com/couchbase/indexing/secondary/logging"
 )
 
 func main() {
@@ -19,13 +21,40 @@ func main() {
 	s := supervisor.NewSuperSupervisor(flags.eventingAdminPort, flags.eventingDir, flags.kvPort, flags.restPort, flags.uuid)
 
 	// For app reloads
-	go metakv.RunObserveChildren(supervisor.MetakvAppsPath, s.EventHandlerLoadCallback, s.CancelCh)
+	go func() {
+		cancelCh := make(chan struct{})
+		for {
+			err := metakv.RunObserveChildren(supervisor.MetakvAppsPath, s.EventHandlerLoadCallback, cancelCh)
+			if err != nil {
+				logging.Errorf("Eventing::main metakv observe error for event handler code, err: %v. Retrying...", err)
+				time.Sleep(2 * time.Second)
+			}
+		}
+	}()
 
 	// For app settings update
-	go metakv.RunObserveChildren(supervisor.MetakvAppSettingsPath, s.SettingsChangeCallback, s.CancelCh)
+	go func() {
+		cancelCh := make(chan struct{})
+		for {
+			err := metakv.RunObserveChildren(supervisor.MetakvAppSettingsPath, s.SettingsChangeCallback, cancelCh)
+			if err != nil {
+				logging.Errorf("Eventing::main metakv observe error for settings, err: %v. Retrying...", err)
+				time.Sleep(2 * time.Second)
+			}
+		}
+	}()
 
 	// For topology change notifications
-	go metakv.RunObserveChildren(supervisor.MetakvRebalanceTokenPath, s.TopologyChangeNotifCallback, s.CancelCh)
+	go func() {
+		cancelCh := make(chan struct{})
+		for {
+			err := metakv.RunObserveChildren(supervisor.MetakvRebalanceTokenPath, s.TopologyChangeNotifCallback, cancelCh)
+			if err != nil {
+				logging.Errorf("Eventing::main metakv observe error for rebalance token, err: %v. Retrying...", err)
+				time.Sleep(2 * time.Second)
+			}
+		}
+	}()
 
 	s.HandleSupCmdMsg()
 }
