7ddf65ea82ce8eef68a91f21fa64f740ec8f9f31
Sriram Melkote <siri@couchbase.com>
2017-09-22 04:57:33 +0530

    Fixing yylex prototype try #2
    Change-Id: I6cc550cd8d399081bc390a2422e3a518548a4723

--- v8_consumer/macros/fixjsify.cmake
+++
@@ -1,4 +0,0 @@
-# This is a temporary script. Flex will be switched to c++ mode soon
-FILE(READ gen/jsify.c JSIFY)
-STRING(REPLACE "extern int yylex" "extern \"C\" int yylex" JSIFY "${JSIFY}")
-FILE(WRITE gen/jsify.cc "${JSIFY}")
