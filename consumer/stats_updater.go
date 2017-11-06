478cc6b4e6a857c580e92288903650c17d887c2f
Abhishek Singh <singhabhishek.bit@gmail.com>
2017-11-06 21:58:45 +0530

    [Rebalance] Subset of plasma records didn't transfer during rebalance
    This primarily showed up in case host name update. In such case, vbucket
    takeover and give up routines ran at same time. Vbucket ownership lead
    to cleaning up of dcp connection associated with old hostname of KV node
    and as a result of all STREAMEND vbucket processing metadata was updated
    as well. This affected the working of vbucket giveup routine which
    relied on that in-memory metadata.
    
    This change basically creates a copy of that in-memory structure, so
    that vbucket giveup routine is unaffected by behaviour of vbucket
    takeover routine.
    
    Change-Id: I1e9f12caea52285b18922b92541fc5a8b4e3c4d4
    Reviewed-on: http://review.couchbase.org/85121
    Tested-by: Abhishek Singh <abhishek@couchbase.com>
    Reviewed-by: Abhishek Singh <abhishek@couchbase.com>

--- consumer/stats_updater.go
+++ consumer/stats_updater.go
@@ -45,3 +45,27 @@ func (vbs vbStats) updateVbStat(vb uint16, statName string, val interface{}) {
 	defer vbstat.Unlock()
 	vbstat.stats[statName] = val
 }
+
+func (vbs vbStats) copyVbStats() vbStats {
+	vbsts := make(vbStats, numVbuckets)
+	for i := uint16(0); i < numVbuckets; i++ {
+		vbsts[i] = &vbStat{
+			stats: make(map[string]interface{}),
+		}
+
+		// params to copy over
+		vbsts[i].stats["assigned_worker"] = vbs.getVbStat(i, "assigned_worker")
+		vbsts[i].stats["current_vb_owner"] = vbs.getVbStat(i, "current_vb_owner")
+		vbsts[i].stats["currently_processed_doc_id_timer"] = vbs.getVbStat(i, "currently_processed_doc_id_timer")
+		vbsts[i].stats["currently_processed_non_doc_timer"] = vbs.getVbStat(i, "currently_processed_non_doc_timer")
+		vbsts[i].stats["dcp_stream_status"] = vbs.getVbStat(i, "dcp_stream_status")
+		vbsts[i].stats["doc_id_timer_processing_worker"] = vbs.getVbStat(i, "doc_id_timer_processing_worker")
+		vbsts[i].stats["last_processed_doc_id_timer_event"] = vbs.getVbStat(i, "last_processed_doc_id_timer_event")
+		vbsts[i].stats["last_processed_seq_no"] = vbs.getVbStat(i, "last_processed_seq_no")
+		vbsts[i].stats["next_doc_id_timer_to_process"] = vbs.getVbStat(i, "next_doc_id_timer_to_process")
+		vbsts[i].stats["next_non_doc_timer_to_process"] = vbs.getVbStat(i, "next_non_doc_timer_to_process")
+		vbsts[i].stats["node_uuid"] = vbs.getVbStat(i, "node_uuid")
+		vbsts[i].stats["plasma_last_persisted_seq_no"] = vbs.getVbStat(i, "plasma_last_persisted_seq_no")
+	}
+	return vbsts
+}
--- consumer/vbucket_takeover.go
+++ consumer/vbucket_takeover.go
@@ -40,7 +40,7 @@ func (c *Consumer) reclaimVbOwnership(vb uint16) error {
 }
 
 // Vbucket ownership give-up routine
-func (c *Consumer) vbGiveUpRoutine() {
+func (c *Consumer) vbGiveUpRoutine(vbsts vbStats) {
 
 	if len(c.vbsRemainingToGiveUp) == 0 {
 		logging.Tracef("CRVT[%s:%s:%s:%d] No vbuckets remaining to give up",
@@ -65,7 +65,7 @@ func (c *Consumer) vbGiveUpRoutine() {
 	wg.Add(c.vbOwnershipGiveUpRoutineCount)
 
 	for i := 0; i < c.vbOwnershipGiveUpRoutineCount; i++ {
-		go func(c *Consumer, i int, vbsRemainingToGiveUp []uint16, signalPlasmaClosedCh chan uint16, wg *sync.WaitGroup) {
+		go func(c *Consumer, i int, vbsRemainingToGiveUp []uint16, signalPlasmaClosedCh chan uint16, wg *sync.WaitGroup, vbsts vbStats) {
 
 			defer wg.Done()
 
@@ -76,13 +76,14 @@ func (c *Consumer) vbGiveUpRoutine() {
 				vbKey := fmt.Sprintf("%s_vb_%s", c.app.AppName, strconv.Itoa(int(vb)))
 				util.Retry(util.NewFixedBackoff(bucketOpRetryInterval), getOpCallback, c, vbKey, &vbBlob, &cas, false)
 
-				logging.Tracef("CRVT[%s:%s:giveup_r_%d:%s:%d] vb: %v uuid: %v vbStat uuid: %v consumer name: %v",
+				logging.Tracef("CRVT[%s:%s:giveup_r_%d:%s:%d] vb: %v uuid: %v vbStat uuid: %v owner node: %v consumer name: %v",
 					c.app.AppName, c.workerName, i, c.tcpPort, c.Pid(), vb, c.NodeUUID(),
-					c.vbProcessingStats.getVbStat(vb, "node_uuid"),
-					c.vbProcessingStats.getVbStat(vb, "assigned_worker"))
+					vbsts.getVbStat(vb, "node_uuid"),
+					vbsts.getVbStat(vb, "current_vb_owner"),
+					vbsts.getVbStat(vb, "assigned_worker"))
 
-				if c.vbProcessingStats.getVbStat(vb, "node_uuid") == c.NodeUUID() &&
-					c.vbProcessingStats.getVbStat(vb, "assigned_worker") == c.ConsumerName() {
+				if vbsts.getVbStat(vb, "node_uuid") == c.NodeUUID() &&
+					vbsts.getVbStat(vb, "assigned_worker") == c.ConsumerName() {
 
 					// TODO: Retry loop for dcp close stream as it could fail and additional verification checks
 					// Additional check needed to verify if vbBlob.NewOwner is the expected owner
@@ -137,7 +138,7 @@ func (c *Consumer) vbGiveUpRoutine() {
 				}
 			}
 
-		}(c, i, vbsDistribution[i], signalPlasmaClosedChs[i], &wg)
+		}(c, i, vbsDistribution[i], signalPlasmaClosedChs[i], &wg, vbsts)
 	}
 
 	wg.Add(1)
@@ -186,7 +187,9 @@ func (c *Consumer) vbsStateUpdate() {
 	c.vbsRemainingToGiveUp = c.getVbRemainingToGiveUp()
 	c.vbsRemainingToOwn = c.getVbRemainingToOwn()
 
-	go c.vbGiveUpRoutine()
+	vbsts := c.vbProcessingStats.copyVbStats()
+
+	go c.vbGiveUpRoutine(vbsts)
 
 	logging.Tracef("CRVT[%s:%s:%s:%d] Before vbTakeover job execution, vbsRemainingToOwn => %v vbRemainingToGiveUp => %v",
 		c.app.AppName, c.workerName, c.tcpPort, c.Pid(),
@@ -367,12 +370,7 @@ func (c *Consumer) updateVbOwnerAndStartDCPStream(vbKey string, vb uint16, vbBlo
 	dTimerDir := fmt.Sprintf("%v/reb_%v_%v_timer.data", c.eventingDir, vb, c.app.AppName)
 
 	if previousEventingDir != c.eventingDir && c.NodeUUID() != previousNodeUUID {
-		if err := client.DownloadDir(timerDir, c.eventingDir); err != nil {
-			logging.Errorf("CRVT[%s:%s:%s:%d] vb: %v Failed to download timer dir from node: %v src: %v dst: %v err: %v",
-				c.app.AppName, c.workerName, c.tcpPort, c.Pid(), vb, remoteConsumerAddr, sTimerDir, dTimerDir, err)
-
-			return errFailedRPCDownloadDir
-		}
+		util.Retry(util.NewFixedBackoff(bucketOpRetryInterval*5), downloadDirCallback, c, client, timerDir, sTimerDir, dTimerDir, remoteConsumerAddr, vb)
 		logging.Debugf("CRVT[%s:%s:%s:%d] vb: %v Successfully downloaded timer dir: %v to: %v from: %v",
 			c.app.AppName, c.workerName, c.tcpPort, c.Pid(), vb, sTimerDir, dTimerDir, remoteConsumerAddr)
 
