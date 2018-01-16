4563be43ab8634cf59dd95e6409cf86af3060246
Abhishek Singh <singhabhishek.bit@gmail.com>
2018-01-16 16:37:32 +0530

    [MB-27561] Avoid firing doc timers ahead of current time
    Change-Id: Ie39b4a17b73243d894e62baf890ebe70f8f5ed0f
    Reviewed-on: http://review.couchbase.org/87893
    Reviewed-by: Abhishek Singh <abhishek@couchbase.com>
    Tested-by: Abhishek Singh <abhishek@couchbase.com>

--- consumer/timer_events.go
+++ consumer/timer_events.go
@@ -200,7 +200,24 @@ func (r *timerProcessingWorker) processTimerEvents(cTimer, nTimer string) {
 			for itr.Seek([]byte(startKeyPrefix)); itr.Valid(); itr.Next() {
 				logging.Debugf("CRTE[%s:%s:timer_%d:%s:%d] vb: %d timerEvent key: %v value: %v",
 					r.c.app.AppName, r.c.workerName, r.id, r.c.tcpPort, r.c.Pid(), vb, string(itr.Key()), string(itr.Value()))
-				r.c.processTimerEvent(currTimer, string(itr.Value()), vb, false, writer)
+
+				entries:=strings.Split(string(itr.Key()), "::")
+
+				// For some reason plasma iterator returned timer entries from future with
+				// correct set of start and end key prefix. Mitigating it via below workaround
+				// until we know the real cause of it
+				if len(entries) == 5 {
+					ts, err := time.Parse(tsLayout, entries[2])
+					if err != nil {
+						continue
+					}
+
+					if ts.After(time.Now()) {
+						continue
+					}
+
+					r.c.processTimerEvent(currTimer, string(itr.Value()), vb, false, writer)
+				}
 			}
 
 			snapshot.Close()
