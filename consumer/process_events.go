2b822e5f85d6f2f81f334e99d012971794b25050
Abhishek Singh <singhabhishek.bit@gmail.com>
2018-04-06 19:47:21 +0530

    MB-28866 Store doc timers from recursive mutation handling as well
    Change-Id: I6f2c8050a42be29f485d88a31ba58027e04a39f3
    Reviewed-on: http://review.couchbase.org/92312
    Reviewed-by: Abhishek Singh <abhishek@couchbase.com>
    Tested-by: Abhishek Singh <abhishek@couchbase.com>

--- consumer/process_events.go
+++ consumer/process_events.go
@@ -8,6 +8,7 @@ import (
 	"hash/crc32"
 	"runtime/debug"
 	"sort"
+	"strings"
 	"time"
 
 	"github.com/couchbase/eventing/common"
@@ -148,8 +149,47 @@ func (c *Consumer) processEvents() {
 
 								logging.Tracef("%s [%s:%s:%d] Sending key: %ru to be stored in plasma",
 									logPrefix, c.workerName, c.tcpPort, c.Pid(), string(e.Key))
+
+								// Enabling it until MB-28779 gets resolved
+								for _, timerEntry := range xMeta.Timers {
+
+									data := strings.Split(timerEntry, "::")
+
+									if len(data) == 3 {
+										pEntry := &plasmaStoreEntry{
+											callbackFn: data[2],
+											key:        string(e.Key),
+											timerTs:    data[1],
+											vb:         e.VBucket,
+										}
+
+										c.plasmaStoreCh <- pEntry
+										logging.Tracef("%s [%s:%s:%d] Sending key: %ru to be stored in plasma, timer entry: %v pEntry: %#v",
+											logPrefix, c.workerName, c.tcpPort, c.Pid(), string(e.Key), timerEntry, pEntry)
+									}
+								}
 							}
 						} else {
+
+							// Enabling it until MB-28779 gets resolved
+							for _, timerEntry := range xMeta.Timers {
+
+								data := strings.Split(timerEntry, "::")
+
+								if len(data) == 3 {
+									pEntry := &plasmaStoreEntry{
+										callbackFn: data[2],
+										key:        string(e.Key),
+										timerTs:    data[1],
+										vb:         e.VBucket,
+									}
+
+									c.plasmaStoreCh <- pEntry
+									logging.Tracef("%s [%s:%s:%d] Sending key: %ru to be stored in plasma, timer entry: %v pEntry: %#v",
+										logPrefix, c.workerName, c.tcpPort, c.Pid(), string(e.Key), timerEntry, pEntry)
+								}
+							}
+
 							logging.Tracef("%s [%s:%s:%d] Skipping recursive mutation for key: %ru vb: %v, xmeta: %ru",
 								logPrefix, c.workerName, c.tcpPort, c.Pid(), string(e.Key), e.VBucket, fmt.Sprintf("%#v", xMeta))
 
