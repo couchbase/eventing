bd5a5ad35106b1098e9dfe802c6d2b986f88d69f
nandsatya <satya.nand@couchbase.com>
2019-03-25 20:35:16 +0530

    MB-33491: Fix concurrent latency stats access
    Change-Id: I3793f27451eb26336946a920af262a9723638ef1
    Reviewed-on: http://review.couchbase.org/106715
    Reviewed-by: Gautham B A <gautham.bangalore@gmail.com>
    Tested-by: <satya.nand@couchbase.com>

--- util/stats.go
+++ util/stats.go
@@ -1,11 +1,16 @@
 package util
 
-import "github.com/couchbase/eventing/common"
+import (
+	"sync"
+
+	"github.com/couchbase/eventing/common"
+)
 
 type Stats struct {
 	stopCh   chan struct{}
 	appendCh chan common.StatsData
 	data     common.StatsData
+	mu       sync.RWMutex
 }
 
 func NewStats() *Stats {
@@ -27,10 +32,18 @@ func (s *Stats) Append(deltas common.StatsData) {
 }
 
 func (s *Stats) Get() common.StatsData {
-	return s.data
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	stats := make(common.StatsData)
+	for key, value := range s.data {
+		stats[key] = value
+	}
+	return stats
 }
 
 func (s *Stats) update(deltas common.StatsData) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	for key, value := range deltas {
 		s.data[key] += value
 	}
