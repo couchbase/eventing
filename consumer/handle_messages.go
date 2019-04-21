9dd19d549a3670194906efeca91cbe79507ee624
Jeelan Basha Poola <jeelan.poola@couchbase.com>
2019-04-21 10:56:47 +0530

    MB-34309: remove write dead line & retry on ErrShortWrite [back port of MB-33484]
    We do not need write dead line / timeout as consumer already has
    logic to terminate a hung isolate, if any. Killing consumer just
    b/c of a write timeout causes all timers in flight to be lost.
    As seen in MB-33484, when curl delay is 200ms, consumer can be slow
    resulting in write timeout on go side. go side should continue
    to write unless it encounters an irrocoverable error like socket
    closure
    
    Change-Id: I48e9d3efedb8559b3fa303017608c278b4677fcf
    Reviewed-on: http://review.couchbase.org/108104
    Reviewed-by: CI Bot
    Reviewed-by: <satya.nand@couchbase.com>
    Tested-by: Jeelan Basha Poola <jeelan.poola@couchbase.com>
    Reviewed-on: http://review.couchbase.org/109633
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Jeelan Basha Poola <jeelan.poola@couchbase.com>

--- consumer/handle_messages.go
+++ consumer/handle_messages.go
@@ -9,7 +9,6 @@ import (
 	"runtime/debug"
 	"strconv"
 	"sync/atomic"
-	"time"
 
 	mcd "github.com/couchbase/eventing/dcp/transport"
 	"github.com/couchbase/eventing/dcp/transport/client"
@@ -487,7 +486,6 @@ func (c *Consumer) sendMessageLoop() {
 					return
 				}
 
-				c.conn.SetWriteDeadline(time.Now().Add(c.socketTimeout))
 
 				func() {
 					c.sendMsgBufferRWMutex.Lock()
@@ -499,7 +497,10 @@ func (c *Consumer) sendMessageLoop() {
 						return
 					}
 
-					_, err := c.sendMsgBuffer.WriteTo(c.conn)
+                                        err := io.ErrShortWrite;
+                                        for  ; err == io.ErrShortWrite; _, err = c.sendMsgBuffer.WriteTo(c.conn) {
+                                        }
+
 					if err != nil {
 						logging.Errorf("%s [%s:%s:%d] stoppingConsumer: %t write to downstream socket failed, err: %v",
 							logPrefix, c.workerName, c.tcpPort, c.Pid(), c.stoppingConsumer, err)
@@ -583,9 +584,11 @@ func (c *Consumer) sendMessage(m *msgToTransmit) error {
 		defer c.connMutex.Unlock()
 
 		if !m.sendToDebugger && c.conn != nil {
-			c.conn.SetWriteDeadline(time.Now().Add(c.socketTimeout))
 
-			_, err := c.sendMsgBuffer.WriteTo(c.conn)
+                        err := io.ErrShortWrite;
+                        for  ; err == io.ErrShortWrite; _, err = c.sendMsgBuffer.WriteTo(c.conn) {
+                        }
+
 			if err != nil {
 				logging.Errorf("%s [%s:%s:%d] stoppingConsumer: %t write to downstream socket failed, err: %v",
 					logPrefix, c.workerName, c.tcpPort, c.Pid(), c.stoppingConsumer, err)
