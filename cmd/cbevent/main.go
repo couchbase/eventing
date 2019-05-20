e9f81163a9f09d6cda6a7b5f755aa63e13ba7487
Sriram Melkote <siri@couchbase.com>
2019-05-21 02:43:57 +0530

    Add ability to pack/unpack code to cbevent
    Run the cbevent with no parameters for examples
    
    Change-Id: I9dc813d66145fe4768b62a0ebf30d2833c25a9ab
    Reviewed-on: http://review.couchbase.org/109393
    Reviewed-by: CI Bot
    Reviewed-by: Jeelan Basha Poola <jeelan.poola@couchbase.com>
    Tested-by: Sriram Melkote <siri@couchbase.com>

--- cmd/cbevent/main.go
+++ cmd/cbevent/main.go
@@ -1,114 +1,152 @@
 package main
 
 import (
-	"crypto/tls"
 	"flag"
 	"fmt"
-	"log"
-	"net/http"
 	"os"
 	"os/exec"
-	"strings"
 	"syscall"
-
-	"github.com/couchbase/cbauth/metakv"
-	"github.com/couchbase/eventing/util"
 )
 
-const eventingPath string = "/eventing/"
-
-const rebalanceTokenPath string = "/eventing/rebalanceToken/"
-const keepNodesPath string = "/eventing/config/keepNodes"
-const settingsConfigPath string = "/eventing/settings/config"
-
-func shouldNotDelete(path string) bool {
-	return strings.HasPrefix(path, rebalanceTokenPath) ||
-		path == keepNodesPath ||
-		path == settingsConfigPath
+type Command struct {
+	List     bool
+	Flush    bool
+	User     string
+	Password string
+	Host     string
+	Pack     bool
+	Unpack   bool
+	Handler  string
+	CodeIn   string
+	CodeOut  string
 }
 
-func list() {
-	children, err := metakv.ListAllChildren(eventingPath)
-	if err != nil {
-		log.Fatalf("Unable to list children, err: %v", err)
-	}
+func usage(fset *flag.FlagSet) {
+	fmt.Fprintln(os.Stderr, "Usage: cbevent [options]")
+	fset.PrintDefaults()
+	fmt.Fprintln(os.Stderr, `
 
-	for _, child := range children {
-		log.Println(child.Path)
-	}
+Examples:
+- Metadata
+    cbevent -list -user Administrator -pass password -host localhost:8091
+    cbevent -flush -user Administrator -pass password -host localhost:8091
+
+- Pack/Unpack
+    cbevent -unpack -handler handler.json -codeout code.js
+    cbevent -pack -handler handler.json -codein code.js
+    `)
 }
 
-func restart(nsServerAddr, username, password string) {
-	svc := "eventingSSL"
-	cinfo, err := util.FetchNewClusterInfoCache(nsServerAddr)
-	if err != nil {
-		log.Fatalf("Unable to list nodes, err: %v", err)
-	}
-	addrs := cinfo.GetNodesByServiceType(svc)
-	nodes := []string{}
-	for _, addr := range addrs {
-		addr, err := cinfo.GetServiceAddress(addr, svc)
-		if err != nil {
-			log.Printf("Ignoring error getting eventing node address: %v", err)
-			continue
-		}
-		nodes = append(nodes, addr)
-	}
-	client := &http.Client{Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}}
+func validate(fset *flag.FlagSet, cmd *Command) error {
+	var have []string
+	var dont []string
 
-	for _, node := range nodes {
-		addr := "https://" + node + "/die"
-		request, err := http.NewRequest("GET", addr, nil)
-		request.SetBasicAuth(username, password)
-		log.Printf("Asking eventing on node %v to die", addr)
-		_, err = client.Do(request)
-		if err != nil {
-			log.Printf("Ignoring error asking eventing on node %v to die: %v", node, err)
-		}
+	switch {
+	// dont = []string{"user", "password", "host", "list", "flush", "unpack", "pack", "codein", "codeout", "handler"}
+
+	case cmd.List:
+		have = []string{"list", "user", "password", "host"}
+		dont = []string{"flush", "unpack", "pack", "codein", "codeout", "handler"}
+
+	case cmd.Flush:
+		have = []string{"flush", "user", "password", "host"}
+		dont = []string{"list", "unpack", "pack", "codein", "codeout", "handler"}
+
+	case cmd.Unpack:
+		have = []string{"unpack", "codeout", "handler"}
+		dont = []string{"user", "password", "host", "list", "flush", "pack", "codein"}
+
+	case cmd.Pack:
+		have = []string{"pack", "codein", "handler"}
+		dont = []string{"user", "password", "host", "list", "flush", "unpack", "codeout"}
+
+	default:
+		return fmt.Errorf("No operation specified")
 	}
+
+	err := mustHave(fset, have...)
+	if err != nil {
+		return err
+	}
+
+	err = mustNotHave(fset, dont...)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func flush(nsServerAddr, username, password string) {
-	children, err := metakv.ListAllChildren(eventingPath)
-	if err != nil {
-		log.Fatalf("Unable to list children, err : %v", err)
-	}
-
-	for _, child := range children {
-		if shouldNotDelete(child.Path) {
-			log.Printf("Skipping deletion of %s", child.Path)
-			continue
+func mustHave(fset *flag.FlagSet, keys ...string) error {
+	for _, key := range keys {
+		found := false
+		fset.Visit(
+			func(f *flag.Flag) {
+				if f.Name == key {
+					found = true
+				}
+			})
+		if !found {
+			flag := fset.Lookup(key)
+			if flag == nil || flag.DefValue == "" {
+				return fmt.Errorf("Invalid flags. Flag '%s' is required for this operation", key)
+			}
 		}
-
-		err = metakv.Delete(child.Path, nil)
-		if err != nil {
-			log.Printf("Unable to delete %s, err : %v", child.Path, err)
-			continue
-		}
-		log.Printf("Deleted %s\n", child.Path)
 	}
-	restart(nsServerAddr, username, password)
+	return nil
+}
+
+func mustNotHave(fset *flag.FlagSet, keys ...string) error {
+	for _, key := range keys {
+		found := false
+		fset.Visit(
+			func(f *flag.Flag) {
+				if f.Name == key {
+					found = true
+				}
+			})
+		if found {
+			return fmt.Errorf("Invalid flags. Flag '%s' cannot appear for this operation", key)
+		}
+	}
+	return nil
 }
 
 func main() {
-	listOption := flag.Bool("list", false,
-		fmt.Sprintf("list all metakv entries under %s", eventingPath))
-	flushOption := flag.Bool("flush", false,
-		fmt.Sprintf("deletes all metakv paths (except the following -\n%s\n%s\n%s) and restarts all eventing nodes",
-			rebalanceTokenPath, keepNodesPath, settingsConfigPath))
+	cmd := Command{}
+	fset := flag.NewFlagSet("cmd", flag.ExitOnError)
 
-	userOption := flag.String("user", "", "Username")
-	pwdOption := flag.String("password", "", "Password")
-	hostOption := flag.String("host", "", "Host:Port of the Couchbase node. Example - localhost:8091")
+	fset.BoolVar(&cmd.List, "list", false, "list all metadata entries")
+	fset.BoolVar(&cmd.Flush, "flush", false, "deletes all metadata that can be deleted")
+	fset.StringVar(&cmd.User, "user", "", "cluster admin username")
+	fset.StringVar(&cmd.Password, "password", "", "cluster admin password")
+	fset.StringVar(&cmd.Host, "host", "", "hostname:port of couchbase console, ex: localhost:8091")
 
-	flag.Parse()
-	if *userOption == "" || *pwdOption == "" || *hostOption == "" {
-		flag.Usage()
-		log.Fatal("Options -user, -password, -host all are necessary")
+	fset.BoolVar(&cmd.Pack, "pack", false, "pack edited code back into specified handler")
+	fset.BoolVar(&cmd.Unpack, "unpack", false, "extracts code from a handler to specified file")
+	fset.StringVar(&cmd.Handler, "handler", "", "handler to extract from or pack into")
+	fset.StringVar(&cmd.CodeIn, "codein", "", "code to read and pack into handler")
+	fset.StringVar(&cmd.CodeOut, "codeout", "", "filename to write extracted code into")
+
+	if len(os.Args) <= 1 {
+		usage(fset)
+		os.Exit(0)
 	}
 
-	if os.Getenv("CBAUTH_REVRPC_URL") == "" {
-		revrpc := fmt.Sprintf("http://%s:%s@%s/_cbauth", *userOption, *pwdOption, *hostOption)
+	err := fset.Parse(os.Args[1:])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	err = validate(fset, &cmd)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
+	}
+
+	if cmd.User != "" && os.Getenv("CBAUTH_REVRPC_URL") == "" {
+		revrpc := fmt.Sprintf("http://%s:%s@%s/_cbauth", cmd.User, cmd.Password, cmd.Host)
 		os.Setenv("CBAUTH_REVRPC_URL", revrpc)
 		cmd := exec.Command(os.Args[0], os.Args[1:]...)
 		cmd.Stdin = os.Stdin
@@ -123,11 +161,14 @@ func main() {
 		os.Exit(exitcode)
 	}
 
-	if *listOption {
+	switch {
+	case cmd.List:
 		list()
-	}
-
-	if *flushOption {
-		flush(*hostOption, *userOption, *pwdOption)
+	case cmd.Flush:
+		flush(cmd.Host, cmd.User, cmd.Password)
+	case cmd.Pack:
+		pack(cmd.Handler, cmd.CodeIn)
+	case cmd.Unpack:
+		unpack(cmd.Handler, cmd.CodeOut)
 	}
 }
