d829c9dcce1384989bfb78e93479ebeb86154569
Jeelan Basha Poola <jeelan.poola@couchbase.com>
2019-09-16 09:19:59 +0000

    Revert "[MB-34948] Do not throw exception for ENOENT bucket op"
    This reverts commit 832efd20de196663b14859a4476c332fa8afa258.
    
    Change-Id: Ie0746101b03af6e4ad0ac71a42efa99a03b6532b
    Reviewed-on: http://review.couchbase.org/114806
    Well-Formed: Build Bot <build@couchbase.com>
    Reviewed-by: Jeelan Basha Poola <jeelan.poola@couchbase.com>
    Tested-by: Jeelan Basha Poola <jeelan.poola@couchbase.com>

--- tests/functional_tests/hcode/bucket_op_enoent_delete.js
+++
@@ -1,8 +0,0 @@
-function OnUpdate(doc, meta) {
-    try {
-        delete dst_bucket['some-non-existent-key'];
-        dst_bucket[meta.id] = 'success';
-    } catch (e) {
-        log('error:', e);
-    }
-}
--- v8_consumer/src/bucket.cc
+++ v8_consumer/src/bucket.cc
@@ -372,11 +372,6 @@ void Bucket::BucketGet<v8::Local<v8::Name>>(
     return;
   }
 
-  if (result.rc == LCB_KEY_ENOENT) {
-    info.GetReturnValue().Set(v8::Undefined(isolate));
-    return;
-  }
-
   // Throw an exception in JavaScript if the bucket get call failed.
   if (result.rc != LCB_SUCCESS) {
     LOG(logTrace) << "Bucket: LCB_GET call failed: " << result.rc << std::endl;
@@ -528,10 +523,6 @@ void Bucket::BucketDelete<v8::Local<v8::Name>>(
                   << lcb_strerror(*bucket_lcb_obj_ptr, err) << std::endl;
     lcb_retry_failure++;
     HandleBucketOpFailure(isolate, *bucket_lcb_obj_ptr, err);
-    return;
-  }
-
-  if (result.rc == LCB_KEY_ENOENT) {
     return;
   }
 
