f1a965213cc93e1894cdaf09628f5e09739f7eb0
Jeelan Basha Poola <jeelan.poola@couchbase.com>
2019-11-20 15:43:27 +0000

    MB-36585 Revert "MB-35581 Fix for SBM data mismatch in volume test"
    This reverts commit 074ca1b3357ce4645536cfc13926e0ceea0fb694.
    
    Change-Id: I7f677a86ea752dd923d822a16182853d14c1af21
    Reviewed-on: http://review.couchbase.org/118221
    Reviewed-by: Jeelan Basha Poola <jeelan.poola@couchbase.com>
    Tested-by: Jeelan Basha Poola <jeelan.poola@couchbase.com>
    Reviewed-by: CI Bot

--- v8_consumer/src/v8worker.cc
+++ v8_consumer/src/v8worker.cc
@@ -529,21 +529,6 @@ void V8Worker::RouteMessage() {
         break;
       }
       break;
-    case eFilter:
-      switch (getFilterOpcode(msg->header.opcode)) {
-      case oProcessedSeqNo: {
-        int vb_no = 0;
-        uint64_t seq_no = 0;
-        if (kSuccess == ParseMetadata(msg->header.metadata, vb_no, seq_no)) {
-          std::lock_guard<std::mutex> guard(bucketops_lock_);
-          UpdateBucketopsSeqno(vb_no, seq_no);
-        }
-      } break;
-      default:
-        LOG(logError) << "Received invalid filter opcode" << std::endl;
-        break;
-      }
-      break;
     default:
       LOG(logError) << "Received unsupported event " << evt << std::endl;
       break;
