3c4621801c064a552e10185d182dd6beaa42b9f1
Chris Hillery <ceej@couchbase.com>
2020-05-13 01:42:06 -0700

    MB-43666: Update header generation to use python 3
    Use the build-provided "cbpy" Python 3 installation, so it no longer depends
    on the build environment.
    
    Also rename the overall "generated" target to "eventing-generated", in case
    it needs to be referenced by other components of the build (the name
    "generated" is just too generic).
    
    Change-Id: Ibc73d6a420aa57bd37150a6c1b65bfe90bb2e27d
    Reviewed-on: http://review.couchbase.org/c/eventing/+/143500
    Reviewed-by: CI Bot
    Reviewed-by: Jeelan Basha Poola <jeelan.poola@couchbase.com>
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- third_party/inspector/compress_json.py
+++ third_party/inspector/compress_json.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import json
 import struct
@@ -9,7 +9,7 @@ if __name__ == '__main__':
   fp = open(sys.argv[1])
   obj = json.load(fp)
   text = json.dumps(obj, separators=(',', ':'))
-  data = zlib.compress(text, zlib.Z_BEST_COMPRESSION)
+  data = zlib.compress(text.encode('utf-8'), zlib.Z_BEST_COMPRESSION)
 
   # To make decompression a little easier, we prepend the compressed data
   # with the size of the uncompressed data as a 24 bits BE unsigned integer.
@@ -17,8 +17,8 @@ if __name__ == '__main__':
   data = struct.pack('>I', len(text))[1:4] + data
 
   step = 20
-  slices = (data[i:i+step] for i in xrange(0, len(data), step))
-  slices = map(lambda s: ','.join(str(ord(c)) for c in s), slices)
+  slices = (data[i:i+step] for i in range(0, len(data), step))
+  slices = map(lambda s: ','.join(str(c) for c in s), slices)
   text = ',\n'.join(slices)
 
   fp = open(sys.argv[2], 'w')
