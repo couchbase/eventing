f41775ba66b0f50c088ad54492f623a1e747bda6
sitaramv <sitaram.vemulapalli@couchbase.com>
2020-08-31 20:56:07 -0700

    MB-38653. Transaction statements.
    Change-Id: I365bb0518ee6cea41557064019d406b363bc15cb
    Reviewed-on: http://review.couchbase.org/c/eventing/+/135345
    Tested-by: Sitaram Vemulapalli <sitaram.vemulapalli@couchbase.com>
    Reviewed-by: Jeelan Basha Poola <jeelan.poola@couchbase.com>

--- parser/n1ql_cc.go
+++ parser/n1ql_cc.go
@@ -5,26 +5,51 @@ import (
 )
 
 func (qs *queryStmt) VisitCreateScope(stmt *algebra.CreateScope) (interface{}, error) {
-        err := handleStmt(qs, stmt.Expressions())
-        return stmt, err
+	err := handleStmt(qs, stmt.Expressions())
+	return stmt, err
 }
 
 func (qs *queryStmt) VisitDropScope(stmt *algebra.DropScope) (interface{}, error) {
-        err := handleStmt(qs, stmt.Expressions())
-        return stmt, err
+	err := handleStmt(qs, stmt.Expressions())
+	return stmt, err
 }
 
 func (qs *queryStmt) VisitCreateCollection(stmt *algebra.CreateCollection) (interface{}, error) {
-        err := handleStmt(qs, stmt.Expressions())
-        return stmt, err
+	err := handleStmt(qs, stmt.Expressions())
+	return stmt, err
 }
 
 func (qs *queryStmt) VisitDropCollection(stmt *algebra.DropCollection) (interface{}, error) {
-        err := handleStmt(qs, stmt.Expressions())
-        return stmt, err
+	err := handleStmt(qs, stmt.Expressions())
+	return stmt, err
 }
 
 func (qs *queryStmt) VisitFlushCollection(stmt *algebra.FlushCollection) (interface{}, error) {
-        err := handleStmt(qs, stmt.Expressions())
-        return stmt, err
+	err := handleStmt(qs, stmt.Expressions())
+	return stmt, err
+}
+
+func (qs *queryStmt) VisitStartTransaction(stmt *algebra.StartTransaction) (interface{}, error) {
+	err := handleStmt(qs, stmt.Expressions())
+	return stmt, err
+}
+
+func (qs *queryStmt) VisitCommitTransaction(stmt *algebra.CommitTransaction) (interface{}, error) {
+	err := handleStmt(qs, stmt.Expressions())
+	return stmt, err
+}
+
+func (qs *queryStmt) VisitRollbackTransaction(stmt *algebra.RollbackTransaction) (interface{}, error) {
+	err := handleStmt(qs, stmt.Expressions())
+	return stmt, err
+}
+
+func (qs *queryStmt) VisitTransactionIsolation(stmt *algebra.TransactionIsolation) (interface{}, error) {
+	err := handleStmt(qs, stmt.Expressions())
+	return stmt, err
+}
+
+func (qs *queryStmt) VisitSavepoint(stmt *algebra.Savepoint) (interface{}, error) {
+	err := handleStmt(qs, stmt.Expressions())
+	return stmt, err
 }
