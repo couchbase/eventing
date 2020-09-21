b724a0207ea2329b4323f1f32ec553bd6130c3d0
AnkitPrabhu <ankit.prabhu@couchbase.com>
2020-09-21 15:21:57 +0530

    MB-40360: Remove lcb error name checking from javascript test code
    Change-Id: I3099103ab4778f898636b495ef6110541a276c67
    Reviewed-on: http://review.couchbase.org/c/eventing/+/136583
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: <ankit.prabhu@couchbase.com>
    Reviewed-by: Jeelan Basha Poola <jeelan.poola@couchbase.com>

--- tests/functional_tests/hcode/advanced_bucket_ops_replace_cas.js
+++ tests/functional_tests/hcode/advanced_bucket_ops_replace_cas.js
@@ -4,7 +4,7 @@ function OnUpdate(doc, meta) {
   // Change the cas
   dst_bucket[meta.id] = 'Changed';
   var {success, error} = couchbase.replace(dst_bucket, meta, 'cas mis match');
-  if(!success && error.cas_mismatch && error.name === 'LCB_KEY_EEXISTS') {
+  if(!success && error.cas_mismatch) {
     var {meta} = couchbase.get(dst_bucket, meta);
 
     var {success} = couchbase.replace(dst_bucket, meta, 'success');
--- tests/functional_tests/hcode/advanced_bucket_ops_replace_enoent.js
+++ tests/functional_tests/hcode/advanced_bucket_ops_replace_enoent.js
@@ -1,9 +1,7 @@
 function OnUpdate(doc, meta) {
   var req = {"id": meta.id + "_Replace"};
   var {success, error} = couchbase.replace(dst_bucket, req, doc);
-  if(!success) {
-    if(error.key_not_found && error.name == "LCB_KEY_ENOENT") {
-      dst_bucket[req.id] = 'success';
-    }
+  if(!success && error.key_not_found) {
+    dst_bucket[req.id] = 'success';
   }
 }
