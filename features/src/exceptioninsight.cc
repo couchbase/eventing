bfc10e43c6971588ab32b5d21f9d212cdc4f65b9
Srini Raman <srinivasan.raman@couchbase.com>
2021-02-10 13:25:12 +0530

    MB-31920: Take care of Windows compilation error
    Change-Id: I24d6a64227984f3dd8bc2dc02dbaa098e2268259
    
    Changes done for MB-31920 as part of
    http://review.couchbase.org/c/eventing/+/145279 broke Windows build
    due to two reasons: (1) a variable named "exception_info" conflicts
    with a prior definition of the same word in VCPP and (2) use of
    localtime_r and asctime_r neither of which is available in VCPP.
    This changeset takes care of both these issues.
    Change-Id: I24d6a64227984f3dd8bc2dc02dbaa098e2268259
    Reviewed-on: http://review.couchbase.org/c/eventing/+/145705
    Tested-by: Srini Raman <srinivasan.raman@couchbase.com>
    Reviewed-by: Jeelan Basha Poola <jeelan.poola@couchbase.com>
    Reviewed-by: CI Bot
    (cherry picked from commit c3ce5af0e24aa07bcc83bc59bbe38f3208af7398)
    Reviewed-on: http://review.couchbase.org/c/eventing/+/145675
    Tested-by: Jeelan Basha Poola <jeelan.poola@couchbase.com>

--- features/src/exceptioninsight.cc
+++ features/src/exceptioninsight.cc
@@ -16,20 +16,20 @@
 void ExceptionInsight::AccumulateException(v8::TryCatch &try_catch) {
 
   auto context = isolate_->GetCurrentContext();
-  auto exception_info = GetExceptionInfo(isolate_, context, &try_catch);
+  auto v8exception_info = GetV8ExceptionInfo(isolate_, context, &try_catch);
 
   // compute a hash for the exception-info to identify duplicates:
   // The field 'stack' contains the exception as well as the stack-track and
   // seems to be the right candidate to check for distinct exceptions.
-  uint64_t crc = crc64_iso.Checksum(reinterpret_cast<const uint8_t *>(exception_info.stack.c_str()),
-    exception_info.stack.length());
+  uint64_t crc = crc64_iso.Checksum(reinterpret_cast<const uint8_t *>(v8exception_info.stack.c_str()),
+    v8exception_info.stack.length());
 
   {
     std::lock_guard<std::mutex> lock(lock_);
     auto &entry = this->entries_[crc];
 
     if (entry.count_ == 0) { // capture first occurrance of the exception
-      entry.exception_info_ = exception_info;
+      entry.v8exception_info_ = v8exception_info;
       entry.count_ = 1;
     } else {
       entry.count_++;
@@ -50,7 +50,7 @@ void ExceptionInsight::AccumulateAndClear(ExceptionInsight &from) {
     auto &entry = this->entries_[iter.first];
 
     if (entry.count_ == 0) { // got a new one not in this instance yet.
-      entry.exception_info_ = iter.second.exception_info_;
+      entry.v8exception_info_ = iter.second.v8exception_info_;
       entry.count_ = iter.second.count_;
     } else {
       entry.count_ += iter.second.count_;
@@ -72,11 +72,11 @@ void ExceptionInsight::LogExceptionSummary(ExceptionInsight &exception_insight) 
 
       exceptionInfo["since"] = exception_insight.start_time_;
       exceptionInfo["count"] = iter.second.count_;
-      exceptionInfo["exception"] = iter.second.exception_info_.exception;
-      exceptionInfo["file"] = iter.second.exception_info_.file;
-      exceptionInfo["line"] = iter.second.exception_info_.line;
-      exceptionInfo["srcLine"] = iter.second.exception_info_.srcLine;
-      exceptionInfo["stack"] = iter.second.exception_info_.stack;
+      exceptionInfo["exception"] = iter.second.v8exception_info_.exception;
+      exceptionInfo["file"] = iter.second.v8exception_info_.file;
+      exceptionInfo["line"] = iter.second.v8exception_info_.line;
+      exceptionInfo["srcLine"] = iter.second.v8exception_info_.srcLine;
+      exceptionInfo["stack"] = iter.second.v8exception_info_.stack;
 
       APPLOG << exceptionInfo.dump() << std::endl;
     }
@@ -98,13 +98,18 @@ void ExceptionInsight::Setup(const std::string &function_name) {
 }
 
 void ExceptionInsight::InitStartTime() {
-    time_t t = time(NULL);
-    struct tm tmbuf;
-    localtime_r(&t, &tmbuf);
-    asctime_r(&tmbuf, start_time_);
+  time_t t;
+  struct tm tmbuf;
 
-    // don't need the NL char at the end of the time string.
-    start_time_[strlen(start_time_) - 1] = '\0';
+  time(&t);
+
+#ifdef WIN32
+  localtime_s(&tmbuf, &t);
+#else
+  localtime_r(&t, &tmbuf);
+#endif
+
+  strftime(start_time_, sizeof(start_time_), "%FT%T",  &tmbuf);
 }
 
 ExceptionInfoEntry::ExceptionInfoEntry(): count_(0) {};
