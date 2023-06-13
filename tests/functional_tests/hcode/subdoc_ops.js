ced76b7f2363c751bed18dfa26107bb42555dff8
AnkitPrabhu <ankit.prabhu@couchbase.com>
2023-06-13 18:00:48 +0530

    MB-57618: Support subdoc array and field operations
    Operations that user can performs are array append, array prepend
    add unique, array insert, upsert, remove, replace and insert
    
    Change-Id: Ica67d576ff2a2e87fb88c1649bac5f9307c79867
    Reviewed-on: https://review.couchbase.org/c/eventing/+/192525
    Reviewed-by: abhishekjindal <abhishek.jindal@couchbase.com>
    Reviewed-by: CI Bot
    Tested-by: <ankit.prabhu@couchbase.com>

---
+++ tests/functional_tests/hcode/subdoc_ops.js
@@ -0,0 +1,53 @@
+function OnUpdate(doc, meta) {
+  meta = {"id": meta.id};
+  couchbase.mutateIn(dst_bucket, meta, [
+    couchbase.MutateInSpec.insert("testField", "insert")
+  ]);
+  doc = dst_bucket[meta.id];
+  if (doc.testField != "insert") {
+    log(doc);
+    return;
+  }
+
+  couchbase.mutateIn(dst_bucket, meta, [
+    couchbase.MutateInSpec.replace("testField", "replace")
+  ]);
+  doc = dst_bucket[meta.id];
+  if (doc.testField != "replace") {
+    log(doc);
+    return;
+  }
+
+  couchbase.mutateIn(dst_bucket, meta, [
+    couchbase.MutateInSpec.remove("testField")
+  ]);
+  doc = dst_bucket[meta.id];
+  if ("testField" in doc) {
+    log(doc);
+    return;
+  }
+
+  couchbase.mutateIn(dst_bucket, meta, [
+    couchbase.MutateInSpec.upsert("arrayTest", []),
+    couchbase.MutateInSpec.arrayAppend("arrayTest", 2),
+    couchbase.MutateInSpec.arrayPrepend("arrayTest", 1),
+    couchbase.MutateInSpec.arrayInsert("arrayTest[0]", 0),
+    //couchbase.MutateInSpec.arrayAddUnique("arrayTest", 1),
+    couchbase.MutateInSpec.arrayAddUnique("arrayTest", 3)
+  ]);
+  doc = dst_bucket[meta.id];
+  var array = doc["arrayTest"];
+
+  if (array.length != 4) {
+    log(doc);
+    return;
+  }
+
+  for(var i = 0; i < 4; i++) {
+      if(array[i] != i) {
+        log(doc);
+        return;
+      }
+  }
+  delete dst_bucket[meta.id];
+}
