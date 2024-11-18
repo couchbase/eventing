a86bba6b639135be71870ea925403cdb87a0e200
AnkitPrabhu <ankit.prabhu@couchbase.com>
2024-11-18 15:52:42 +0530

    MB-56835: Implement dcp manager
    * dcp_manager is a wrapper around dcp_connecion which is used to
    create connection with memcached
    
    Change-Id: I8363dac82e8b8898130e2d42e3c5e85ffff1b746
    Reviewed-on: https://review.couchbase.org/c/eventing/+/186276
    Reviewed-by: Rishit Chaudhary <rishit.chaudhary@couchbase.com>
    Reviewed-by: Hiren <hiren.bavaskar@couchbase.com>
    Tested-by: <ankit.prabhu@couchbase.com>

---
+++ dcp_manager/dcp_manager.go
@@ -0,0 +1,24 @@
+package dcpManager
+
+import (
+	"github.com/couchbase/eventing/dcp_connection"
+)
+
+// DcpManager manages dcp connection
+// It will send the dcp messages to the receiver
+// It will manage all the connection till caller calls
+// close dcp
+type DcpManager interface {
+	DoneDcpEvent(event *dcpConn.DcpEvent)
+	StartStreamReq(sr *dcpConn.StreamReq) error
+	PauseStreamReq(sr *dcpConn.StreamReq)
+	CloseRequest(sr *dcpConn.StreamReq) (*dcpConn.StreamReq, error)
+	GetFailoverLog(vbs []uint16) (map[uint16]dcpConn.FailoverLog, error)
+	GetSeqNumber(vbs []uint16, collectionID string) (map[uint16]uint64, error)
+
+	RegisterID(id uint16, sendChannel chan<- *dcpConn.DcpEvent)
+	DeregisterID(id uint16)
+
+	CloseManager()
+	CloseConditional() bool
+}
